cc61a363db1b2edb1a437ffbe7c4f77157afba8f
sandeep hs <sandeepx.hs@intel.com>
2018-04-13 20:58:52 +0530

    block_info : test case for handler and config file

--- families/block_info/tests/test_tp_block_info_config.py
+++ families/block_info/tests/test_tp_block_info_config.py
@@ -1,75 +1,70 @@
-import time
-import zmq
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
 import unittest
 
-
-
-from sawtooth_processor_test.message_factory import MessageFactory
-from sawtooth_block_info.protobuf.block_info_pb2 import BlockInfoTxn
-from sawtooth_block_info.protobuf.block_info_pb2 import BlockInfo
-from sawtooth_block_info.protobuf.block_info_pb2 import BlockInfoConfig
+from sawtooth_block_info.processor.config.block_info \
+import load_default_block_info_config,\
+    load_toml_block_info_config, merge_block_info_config
 from sawtooth_sdk.processor.exceptions import LocalConfigurationError
-from sawtooth_block_info.processor.config.block_info import load_default_block_info_config, load_toml_block_info_config, merge_block_info_config
-from sawtooth_block_info.common import FAMILY_NAME
-from sawtooth_block_info.common import FAMILY_VERSION
-from sawtooth_block_info.common import NAMESPACE
-from sawtooth_block_info.common import CONFIG_ADDRESS
-from sawtooth_block_info.common import DEFAULT_SYNC_TOLERANCE
-from sawtooth_block_info.common import DEFAULT_TARGET_COUNT
-from sawtooth_block_info.common import create_block_address
-
-
 
 class TestBlockInfoConfig(unittest.TestCase):
     def test_load_default_block_info_config(self):
         """ Tests the default return value of config file """
-        bc=load_default_block_info_config()
-        self.assertEqual(bc.connect, "tcp://localhost:4004")
-        #self.assertEqual(bc.connect, "hi")
-        
+        block_config_default = load_default_block_info_config()
+        self.assertEqual(block_config_default.connect, "tcp://localhost:4004")
+
     def test_load_toml_block_info_config_ne(self):
         """ Tests toml load file if it is not present """
-        filename="test.toml"
-        cf=load_toml_block_info_config(filename)
-        self.assertEqual(cf.connect, None)
-    
+        filename = "test.toml"
+        block_config_default = load_toml_block_info_config(filename)
+        self.assertEqual(block_config_default.connect, None)
+
     def test_load_toml_block_info_config(self):
         """ Tests value inside toml load file """
-        filename="test_block_info.toml"
+        filename = "test_block_info.toml"
         with open(filename, 'w') as fd:
             fd.write('connect = "tcp://blarg:1234"')
-        config = load_toml_block_info_config(filename)
-        self.assertEqual(config.connect, "tcp://blarg:1234")
+        block_config_filename = load_toml_block_info_config(filename)
+        self.assertEqual(block_config_filename.connect, "tcp://blarg:1234")
 
     def test_merge_config(self):
-            """ Tests the merge of all toml config files """
-            L=[]
-            bc1=load_default_block_info_config()
-            bc2=load_default_block_info_config()
-            bc3=load_default_block_info_config()
-            L.append(bc1)
-            L.append(bc2)
-            L.append(bc3)
-            mc=merge_block_info_config(L)
-            self.assertEqual(mc.connect, "tcp://localhost:4004")
-            
+        """ Tests the merge of all toml config files """
+        l = []
+        block_config_default_1 = load_default_block_info_config()
+        block_config_default_2 = load_default_block_info_config()
+        block_config_default_3 = load_default_block_info_config()
+        l.append(block_config_default_1)
+        l.append(block_config_default_2)
+        l.append(block_config_default_3)
+        mc = merge_block_info_config(l)
+        self.assertEqual(mc.connect, "tcp://localhost:4004")
+
     def test_to_toml(self):
-        tt=load_default_block_info_config()
-        self.assertEqual(tt.to_toml_string(), ['connect = "tcp://localhost:4004"'])
-            
+        block_config_default = load_default_block_info_config()
+        self.assertEqual(block_config_default.to_toml_string(),
+                        ['connect = "tcp://localhost:4004"'])
+
     def test_repr(self):
-        repr=load_default_block_info_config()
-        self.assertEqual(repr.__repr__(), "BlockInfoConfig(connect='tcp://localhost:4004')")
-        
+        block_config_default = load_default_block_info_config()
+        self.assertEqual(block_config_default.__repr__(),
+                         "BlockInfoConfig(connect='tcp://localhost:4004')")
+
     def test_load_toml_block_info_config_invalidkeys(self):
-        filename="a.toml"
+        filename = "a.toml"
         with open(filename, 'w') as fd:
             fd.write('ty = "tcp://test:4004"')
         with self.assertRaises(LocalConfigurationError):
-            config = load_toml_block_info_config(filename)
-          
-    
-  
-        
-    
-
+            load_toml_block_info_config(filename)
--- families/block_info/tests/test_tp_handler.py
+++ families/block_info/tests/test_tp_handler.py
@@ -1,24 +1,25 @@
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
 import time
-import zmq
 import unittest
 
-
-
-
-from sawtooth_processor_test.message_factory import MessageFactory
-from sawtooth_block_info.protobuf.block_info_pb2 import BlockInfoTxn
+from sawtooth_block_info.processor.handler \
+import validate_hex, validate_timestamp
 from sawtooth_block_info.protobuf.block_info_pb2 import BlockInfo
-from sawtooth_block_info.protobuf.block_info_pb2 import BlockInfoConfig
-from sawtooth_block_info.processor.handler import BlockInfoTransactionHandler, validate_hex, validate_timestamp
-from sawtooth_block_info.common import FAMILY_NAME
-from sawtooth_block_info.common import FAMILY_VERSION
-from sawtooth_block_info.common import NAMESPACE
-from sawtooth_block_info.common import CONFIG_ADDRESS
 from sawtooth_block_info.common import DEFAULT_SYNC_TOLERANCE
-from sawtooth_block_info.common import DEFAULT_TARGET_COUNT
-from sawtooth_block_info.common import create_block_address
-from sawtooth_sdk.processor.exceptions import InvalidTransaction
-
 
 def create_block_info(block_num,
                       signer_public_key="2" * 66,
@@ -36,34 +37,34 @@ def create_block_info(block_num,
         previous_block_id=previous_block_id)
 
 class TestHandler(unittest.TestCase):
-    
+
     def test_validate_hex_previd(self):
         """ Tests previous block id is in valid hex """
         block_info = create_block_info(block_num=1)
-        vp=validate_hex(block_info.previous_block_id, 128)
-        self.assertEqual(vp, True)
-        
+        previd_hex = validate_hex(block_info.previous_block_id, 128)
+        self.assertEqual(previd_hex, True)
+
     def test_validate_hex_sign_public_key(self):
         """ Tests signer public key is in valid hex """
         block_info = create_block_info(block_num=1)
-        vp=validate_hex(block_info.signer_public_key, 66)
-        self.assertEqual(vp, True)
-        
+        public_key_hex = validate_hex(block_info.signer_public_key, 66)
+        self.assertEqual(public_key_hex, True)
+
     def test_validate_hex_header_sign_key(self):
         """ Tests header signature is in valid hex """
         block_info = create_block_info(block_num=1)
-        vp=validate_hex(block_info.header_signature, 128)
-        self.assertEqual(vp, True)
-        
+        header_signature_hex = validate_hex(block_info.header_signature, 128)
+        self.assertEqual(header_signature_hex, True)
+
     def test_validate_timestamp(self):
         """ Tests the timestamp is greater than zero """
         block_info = create_block_info(block_num=1)
         now = time.time()
         if (block_info.timestamp - now) < DEFAULT_SYNC_TOLERANCE:
-            validate_timestamp(block_info.timestamp,DEFAULT_SYNC_TOLERANCE)
+            validate_timestamp(block_info.timestamp, DEFAULT_SYNC_TOLERANCE)
             self.assertTrue(True)
-          
+
     def test_validate_hex_ve(self):
-        vp=validate_hex("test", 128)
-        self.assertEqual(vp, False)
+        string_hex = validate_hex("test", 128)
+        self.assertEqual(string_hex, False)
    
