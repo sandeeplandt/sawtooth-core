6de37e1fb5c7aa54374faf1bcc73cc14d15517ee
aditya singh <adityasingh27@hotmail.com>
2018-07-17 01:10:15 -0700

    resolving merge conflicts

--- rest_api/tests/api_test/base.py
+++ rest_api/tests/api_test/base.py
@@ -1,7 +1,18 @@
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-
-class RestApiBaseTest(object):
-=======
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+  
 from base64 import b64decode
 
 class RestApiBaseTest(object):
@@ -12,7 +23,7 @@ class RestApiBaseTest(object):
         pass
     
     def assert_check_txn_nonce(self, txn , expected_id):
-        expected_id == txn['header']['nonce'] 
+        expected_id == txn['header']['nonce']
     
     def assert_check_family(self, response):
         assert 'family_name' in response
@@ -27,6 +38,7 @@ class RestApiBaseTest(object):
     
     def assert_check_payload_algo(self ,response):
         assert 'payload_sha512' in response
+        
     
     def assert_check_payload(self, response):
         assert 'payload' in response
@@ -54,19 +66,13 @@ class RestApiBaseTest(object):
     def assert_check_block_num(self):
         pass
     
->>>>>>> Test files added for Rest Api testing
     def assert_items(self, items, cls):
             """Asserts that all items in a collection are instances of a class
             """
             for item in items:
                 assert isinstance(item, cls)
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-    
-    def assert_has_valid_head(self, response, expected):
-=======
      
     def assert_valid_head(self, response, expected):
->>>>>>> Test files added for Rest Api testing
             """Asserts a response has a head string with an expected value
             """
             assert 'head' in response
@@ -74,16 +80,6 @@ class RestApiBaseTest(object):
             assert isinstance(head, str)
             assert head == expected
     
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-    def assert_has_valid_link(self, response, expected_ending):
-            """Asserts a response has a link url string with an expected ending
-            """
-            assert link in response['link']
-            self.assert_valid_url(link, expected_ending)
-            
-    
-    def assert_has_valid_paging(self, js_response, pb_paging,
-=======
     def assert_valid_link(self, response, expected):
             """Asserts a response has a link url string with an expected ending
             """
@@ -91,7 +87,6 @@ class RestApiBaseTest(object):
             self.assert_valid_url(link, expected)
             
     def assert_valid_paging(self, js_response, pb_paging,
->>>>>>> Test files added for Rest Api testing
                                     next_link=None, previous_link=None):
             """Asserts a response has a paging dict with the expected values.
             """
@@ -107,11 +102,7 @@ class RestApiBaseTest(object):
             else:
                 assert 'next' not in js_paging
     
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-    def assert_has_valid_error(self, response, expected_code):
-=======
     def assert_valid_error(self, response, expected_code):
->>>>>>> Test files added for Rest Api testing
             """Asserts a response has only an error dict with an expected code
             """
             assert 'error' in response
@@ -125,11 +116,7 @@ class RestApiBaseTest(object):
             assert 'message' in error
             assert isinstance(error['message'], str)
     
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-    def assert_has_valid_data_list(self, response, expected_length):
-=======
     def assert_valid_data_list(self, response, expected_length):
->>>>>>> Test files added for Rest Api testing
         """Asserts a response has a data list of dicts of an expected length.
         """
         assert 'data' in response
@@ -138,11 +125,7 @@ class RestApiBaseTest(object):
         assert expected_length == len(data)
         self.assert_items(data, dict)
     
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-    def assert_has_valid_url(self, url, expected_ending=''):
-=======
     def assert_valid_url(self, url, expected_ending=''):
->>>>>>> Test files added for Rest Api testing
         """Asserts a url is valid, and ends with the expected value
         """
         assert isinstance(url, str)
@@ -150,98 +133,6 @@ class RestApiBaseTest(object):
         assert url.endswith(expected_ending)
      
                         
-<<<<<<< cca345ededcd7d2583019df679d1acb5d0736390
-    def aasert_check_block_seq(blocks, *expected_ids):
-        if not isinstance(blocks, list):
-                blocks = [blocks]
-        
-        consensus = None
-    
-        for block, expected_id in zip(blocks, expected_ids):
-            assert isinstance(block, dict)
-            assert expected_id == block['header_signature']
-            assert isinstance(block['header'], dict)
-            assert consensus ==  b64decode(block['header']['consensus'])
-    
-            batches = block['batches']
-            assert isinstance(batches, list)
-            assert len(batches) == 1
-            assert isinstance(batches, dict)
-            assert check_batch_seq(batches, expected_id)
-        
-        return True
-    
-    def assert_check_batch_seq(signer_key , batches , *expected_ids):
-        if not isinstance(batches, list):
-                batches = [batches]
-    
-        for batch, expected_id in zip(batches, expected_ids):
-            assert expected_id == batch['header_signature']
-            assert isinstance(batch['header'], dict)
-            
-    
-            txns = batch['transactions']
-            assert isinstance(txns, list)
-            assert len(txns) == 1
-            assert isinstance(txns, dict)
-            assert check_transaction_seq(txns, expected_id) == True
-        
-        return True
-    
-    def assert_check_transaction_seq(txns , *expected_ids):
-        if not isinstance(txns, list):
-                txns = [txns]
-        
-        payload = None
-    
-        for txn, expected_id in zip(txns, expected_ids):
-            assert expected_id == txn['header_signature']
-            assert payload == b64decode(txn['payload'])
-            assert isinstance(txn['header'], dict)
-            assert expected_id == txn['header']['nonce']
-        
-        return True
-    
-    def assert_check_batch_nonce(self, response):
-        pass
-    
-    def assert_check_family(self, response):
-        assert 'family_name' in response
-        assert 'family_version' in response
-    
-    def assert_check_dependency(self, response):
-        pass
-    
-    def assert_check_content(self, response):
-        pass
-    
-    def assert_check_payload_algo(self):
-        pass
-    
-    def assert_check_payload(self, response):
-        pass
-    
-    def assert_batcher_public_key(self, signer_key , batch):
-        assert 'public_key' == batch['header']['signer_public_key']
-    
-    def assert_signer_public_key(self, signer_key , batch):
-        assert 'public_key' == batch['header']['signer_public_key']
-    
-    def aasert_check_batch_trace(self, trace):
-        assert bool(trace)
-    
-    def assert_check_consensus(self):
-        pass
-    
-    def assert_state_root_hash(self):
-        pass
-    
-    def assert_check_previous_block_id(self):
-        pass
-    
-    def assert_check_block_num(self):
-        pass
-=======
     def assert_check_block_seq(self, blocks, expected_blocks, expected_batches, expected_txns):
         if not isinstance(blocks, list):
                 blocks = [blocks]
@@ -310,4 +201,3 @@ class RestApiBaseTest(object):
         
     def assert_check_state_seq(self, state, expected):
         pass
->>>>>>> Test files added for Rest Api testing
